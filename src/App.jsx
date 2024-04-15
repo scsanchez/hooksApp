73cf4a4fe10ec12fcf3b67dd45c7b37c3264d3c2
Sergio <sergiocarrascal000@gmail.com>
2024-04-15 09:33:26 +0200

    counter

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./App.css";
+
+export const App = () => {
+  const [state, setCounter] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
+
+  const { counter1, counter2, counter3 } = state;
+
+  return (
+    <>
+      <h1>Counter1: {counter1}</h1>
+      <h1>Counter2: {counter2}</h1>
+      <h1>Counter3: {counter3}</h1>
+      <hr />
+      <button
+        onClick={() =>
+          setCounter({
+            ...state,
+            counter1: counter1 + 1,
+          })
+        }
+      >
+        count is {counter1}
+      </button>
+    </>
+  );
+};
+
+export default App;
